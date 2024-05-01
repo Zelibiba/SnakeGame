e856d2360a31de735c346632be8b75651df69d0d
Дмитрий Дугин <dugin-di@yandex.ru>
2024-05-01 20:00:39 +0300

    Добавлена логика игры без таймера.

--- SnakeGame/Program.fs
+++ SnakeGame/Program.fs
@@ -9,23 +9,23 @@ type Game =
     mutable Direction: Coords
     mutable Apple: Coords
     }
-
 type GameState = 
     | Running of Game
     | End
 
-type Symbols =
+
+type Symbol =
+    | Space
     | SnakeHead
     | SnakeTail
     | Apple
-
 let drawChar color char (row, column) =
     Console.ForegroundColor <- color
     Console.SetCursorPosition (column, row)
     Console.Write(char: char)
-
 let drawSymbol symbol =
     match symbol with
+        | Space     -> drawChar ConsoleColor.White  ' '
         | SnakeHead -> drawChar ConsoleColor.Yellow 'O'
         | SnakeTail -> drawChar ConsoleColor.Yellow 'o'
         | Apple     -> drawChar ConsoleColor.Green  '@'
@@ -42,9 +42,9 @@ let initWalls rows columns =
     wall |> Array.iter (drawChar ConsoleColor.Red '\u2551')
     Array.append walls wall
 let initSnake row column = 
-    let snake = [| (row+1, column); (row, column) |]
-    drawSymbol SnakeHead snake[1] 
-    drawSymbol SnakeTail snake[0]
+    let snake = [| (row, column); (row+1, column) |]
+    drawSymbol SnakeHead snake[0] 
+    drawSymbol SnakeTail snake[1]
     snake
 let initAppleFactory rows columns =
     let createApple appleSeq snake =
@@ -72,10 +72,54 @@ let init rows columns =
     Game
 
 
+let getCommand() =
+    match Console.ReadKey(false).Key with
+        | ConsoleKey.UpArrow -> (-1, 0)
+        | ConsoleKey.RightArrow -> (0, 1)
+        | ConsoleKey.DownArrow -> (1, 0)
+        | ConsoleKey.LeftArrow -> (0, -1)
+        //| _ -> None
+let eatApple (snake: Coords array) apple=
+    drawSymbol SnakeTail snake[0]
+    let snake' = Array.insertAt 0 apple snake
+    drawSymbol SnakeHead snake'[0]
+    snake'
+let move (snake: Coords array) coordsAhead =
+    drawSymbol Space snake[snake.Length-1]
+    for i = snake.Length-1 downto 1 do
+        snake[i] <- snake[i-1]
+    drawSymbol SnakeTail snake[0]
+    snake[0] <- coordsAhead
+    drawSymbol SnakeHead snake[0]
+    snake
+let run (game: Game): Result<Game,unit> =
+    let (dirR, dirC) = getCommand()
+    let (headR, headC) = game.Snake[0]
+    let coordsAhead = (headR + dirR, headC + dirC)
+    if coordsAhead = game.Apple then
+        game.Snake <- eatApple game.Snake game.Apple
+        game.Apple <- game.CreateApple game.Snake
+        Ok game
+    elif Array.contains coordsAhead game.Walls then
+        Error()
+    else
+        game.Snake <- move game.Snake coordsAhead
+        Ok game
+        
+
+
+let rec play (gameState: Result<Game, unit>) =
+    match gameState with
+        | Error _ -> ()
+        | Ok game -> play (run game)
+
+
 [<EntryPoint>]
 let main _ =
     let rows, columns = 30, 30
-    let Game = init rows columns
+    let game = init rows columns
+
+    play (Ok game) |> ignore
 
     drawChar ConsoleColor.White ' ' (rows+2, 0)
     Console.ResetColor()
