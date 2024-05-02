8bc784024f3b7a97928bbeb6c0c34819490f2937
Дмитрий Дугин <dugin-di@yandex.ru>
2024-05-02 23:20:05 +0300

    Обновлён ввод комманд и отрисовка змеи.

--- SnakeGame/Program.fs
+++ SnakeGame/Program.fs
@@ -17,7 +17,7 @@ type GameState =
 type Symbol =
     | Space
     | SnakeHead
-    | SnakeTail
+    | SnakeTail of char
     | Apple
 let drawChar color char (row, column) =
     Console.ForegroundColor <- color
@@ -26,9 +26,42 @@ let drawChar color char (row, column) =
 let drawSymbol symbol =
     match symbol with
         | Space     -> drawChar ConsoleColor.White  ' '
-        | SnakeHead -> drawChar ConsoleColor.Yellow 'O'
-        | SnakeTail -> drawChar ConsoleColor.Yellow 'o'
+        | SnakeHead -> drawChar ConsoleColor.Yellow '\u2588'
+        | SnakeTail char -> drawChar ConsoleColor.Yellow char
         | Apple     -> drawChar ConsoleColor.Green  '@'
+
+type SnakeBodyShape =
+    | Up
+    | Right
+    | Down
+    | Left
+let getSnakeDirection (midR, midC) (edgeR, edgeC) =
+    if midR = edgeR then
+        if edgeC > midC
+            then Right
+            else Left
+    elif midC = edgeC then
+        if edgeR > midR
+            then Down
+            else Up
+    else failwith "Snake body is cutted!"
+let (|MATCH|_|) sample (shape1, shape2) =
+    if ((shape1, shape2) = sample) || ((shape2, shape1) = sample)
+        then Some sample
+        else None
+let drawSnakeHead (snake: Coords array) =
+    let shape = (getSnakeDirection snake[1] snake[0],
+                 getSnakeDirection snake[1] snake[2])
+    let char = match shape with
+               | MATCH (Up, Down) _ -> '\u2502'
+               | MATCH (Left, Right) _ -> '\u2500'
+               | MATCH (Up, Right) _ -> '\u2514'
+               | MATCH (Up, Left) _ -> '\u2518'
+               | MATCH (Down, Right) _ -> '\u250C'
+               | MATCH (Down, Left) _ -> '\u2510'
+               | _ -> 'x'
+    drawSymbol SnakeHead snake[0]
+    drawSymbol (SnakeTail char) snake[1]
 
 
 let initWalls rows columns =
@@ -36,15 +69,14 @@ let initWalls rows columns =
     drawChar ConsoleColor.Red '\u2557' (0, columns+1)
     drawChar ConsoleColor.Red '\u255A' (rows+1, 0)
     drawChar ConsoleColor.Red '\u255D' (rows+1, columns+1)
-    let walls = Array.allPairs [| 0; rows+1 |] [| 1.. columns |]
-    walls |> Array.iter (drawChar ConsoleColor.Red '\u2550')
-    let wall = Array.allPairs [| 1.. rows |] [| 0; columns+1 |]
-    wall |> Array.iter (drawChar ConsoleColor.Red '\u2551')
-    Array.append walls wall
+    let wallH = Array.allPairs [| 0; rows+1 |] [| 1.. columns |]
+    wallH |> Array.iter (drawChar ConsoleColor.Red '\u2550')
+    let wallV = Array.allPairs [| 1.. rows |] [| 0; columns+1 |]
+    wallV |> Array.iter (drawChar ConsoleColor.Red '\u2551')
+    Array.append wallH wallV
 let initSnake row column = 
-    let snake = [| (row, column); (row+1, column) |]
-    drawSymbol SnakeHead snake[0] 
-    drawSymbol SnakeTail snake[1]
+    let snake = [| (row, column); (row+1, column); (row+2, column) |]
+    drawSnakeHead snake
     snake
 let initAppleFactory rows columns =
     let createApple appleSeq snake =
@@ -60,6 +92,7 @@ let initAppleFactory rows columns =
 let init rows columns =
     Console.SetWindowSize (columns+50, rows+20)
     Console.SetBufferSize (columns+50, rows+20)
+    Console.CursorVisible <- false
     let Game =
         {
         Walls = initWalls rows columns
@@ -72,28 +105,39 @@ let init rows columns =
     Game
 
 
-let getCommand() =
-    match Console.ReadKey(false).Key with
-        | ConsoleKey.UpArrow -> (-1, 0)
-        | ConsoleKey.RightArrow -> (0, 1)
-        | ConsoleKey.DownArrow -> (1, 0)
-        | ConsoleKey.LeftArrow -> (0, -1)
-        //| _ -> None
-let eatApple (snake: Coords array) apple=
-    drawSymbol SnakeTail snake[0]
+let getDirection (dirR, dirC) =
+    seq { while Console.KeyAvailable do
+            match Console.ReadKey(false).Key with
+                | ConsoleKey.UpArrow -> Some (-1, 0)
+                | ConsoleKey.RightArrow -> Some (0, 1)
+                | ConsoleKey.DownArrow -> Some (1, 0)
+                | ConsoleKey.LeftArrow -> Some (0, -1)
+                | _ -> None }
+    |> Seq.rev
+    |> Seq.skipWhile (function
+                        | None -> true
+                        | Some (r, c) -> (r = dirR) || (c = dirC) )
+    |> Seq.tryHead
+    |> function
+        | Some value -> value
+        | None -> None
+let eatApple (snake: Coords array) apple =
     let snake' = Array.insertAt 0 apple snake
-    drawSymbol SnakeHead snake'[0]
+    drawSnakeHead snake'
     snake'
 let move (snake: Coords array) coordsAhead =
     drawSymbol Space snake[snake.Length-1]
     for i = snake.Length-1 downto 1 do
         snake[i] <- snake[i-1]
-    drawSymbol SnakeTail snake[0]
     snake[0] <- coordsAhead
-    drawSymbol SnakeHead snake[0]
+    drawSnakeHead snake
     snake
 let run (game: Game): Result<Game,unit> =
-    let (dirR, dirC) = getCommand()
+    Threading.Thread.Sleep(150)
+    match getDirection game.Direction with
+        | Some direction -> game.Direction <- direction
+        | None -> ()
+    let (dirR, dirC) = game.Direction
     let (headR, headC) = game.Snake[0]
     let coordsAhead = (headR + dirR, headC + dirC)
     if coordsAhead = game.Apple then
@@ -101,6 +145,8 @@ let run (game: Game): Result<Game,unit> =
         game.Apple <- game.CreateApple game.Snake
         Ok game
     elif Array.contains coordsAhead game.Walls then
+        Error()
+    elif Array.contains coordsAhead game.Snake then
         Error()
     else
         game.Snake <- move game.Snake coordsAhead
@@ -116,11 +162,11 @@ let rec play (gameState: Result<Game, unit>) =
 
 [<EntryPoint>]
 let main _ =
-    let rows, columns = 30, 30
+    let rows, columns = 20, 20
     let game = init rows columns
 
-    play (Ok game) |> ignore
+    play (run game) |> ignore
 
-    drawChar ConsoleColor.White ' ' (rows+2, 0)
+    drawChar ConsoleColor.White ' ' (rows+3, 0)
     Console.ResetColor()
     0
